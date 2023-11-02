1637e5cfddacb0f5664be1d5700f18a71d159156
A <movie3342@gmail.com>
2023-11-02 21:38:18 +0900

    feat : first svelte

---
+++ README.md
@@ -0,0 +1,29 @@
+# svelte
+
+## dev env
+
+- requirement "bun 1.0.7"
+
+## how to install app
+
+1. first you clone this repo
+
+```zsh
+    git clone <repo url>
+```
+
+2. If your computer not installed Bun, please install first.
+
+3. If you have Bun. You put into Terminal or CLI tool below command.
+
+```zsh
+bun install
+```
+
+4. And...
+
+```zsh
+bun dev
+```
+
+5. Happy Svelte.
---
+++ src/App.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  let userInfo: { id: string; password: string } = { id: '', password: '' };
+  function handleSubmit(e: SubmitEvent) {
+    e.preventDefault();
+
+    if (!e.currentTarget) {
+      return;
+    }
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+    const obj = Object.fromEntries(formData) as { id: string; password: string };
+    userInfo = obj;
+  }
+</script>
+
+<main>
+  <h1>오우야 svelte</h1>
+  <div class="input-container">
+    {#if userInfo.id && userInfo.password}
+      <h2>{userInfo.id}</h2>
+      <h2>{userInfo.password}</h2>
+    {/if}
+  </div>
+
+  <form on:submit={handleSubmit}>
+    <div class="form-item-container">
+      <label for="id">아이디</label>
+      <input id="id" type="text" name="id" />
+    </div>
+    <div class="form-item-container">
+      <label for="password">비밀번호</label>
+      <input type="password" name="password" />
+    </div>
+
+    <button>로그인</button>
+  </form>
+</main>
+
+<style>
+  h1 {
+    margin: 0;
+  }
+  div.input-container {
+    display: flex;
+    gap: 10px;
+  }
+  div.input-container > h2 {
+    font-size: 12px;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+
+  form > div.form-item-container {
+    display: grid;
+    grid-template-columns: 60px 1fr;
+    gap: 8px;
+  }
+  form > div.form-item-container > label {
+    text-align: left;
+    width: 100%;
+  }
+</style>
